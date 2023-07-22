6b9d776eb0184e605cef0740d55f2be4089497a2
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-07-22 19:09:18 +0530

    [Riya] Add : Reverse each word in string

--- PracticeProblems/Program.cs
+++ PracticeProblems/Program.cs
@@ -12,7 +12,7 @@ namespace PracticeProblems
                 Console.WriteLine("Enter input to be executed\n 1.Count No.of duplicate elements in an array\n " +
                     "2.Print all unique elements in an array\n 3.Count the frequency of each element of an array\n " +
                     "4.Find maximum and minimum element in an array\n 5.Print pattern\n 6.Print second pattern\n " +
-                    "7.Exit\n");
+                    "7.Reverse each word in a string\n 8.Exit\n");
                 int input = Convert.ToInt32(Console.ReadLine());
                 switch (input)
                 {
@@ -41,6 +41,10 @@ namespace PracticeProblems
                         printPattern2.PrintGivenPattern2();
                         break;
                     case 7:
+                        ReverseEachWord reverseEachWord = new ReverseEachWord();
+                        reverseEachWord.PrintReverseEachWord();
+                        break;
+                    case 8:
                         flag = false;
                         break;
                 }
