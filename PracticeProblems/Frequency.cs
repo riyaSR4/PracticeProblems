5286d5a8d469d4d4f0a6ffafd5d7f59680278536
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-07-22 17:27:12 +0530

    [Riya] Add : Count frequency of elements

---
+++ PracticeProblems/Frequency.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    public class Frequency
+    {
+        public void CountFrequency()
+        {
+            Console.WriteLine("Enter the limit of array:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] ar = new int[n];
+            Console.WriteLine("Enter the elements:");
+            for (int i = 0; i < ar.Length; i++)
+            {
+                ar[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            var dict = new Dictionary<int, int>();
+            foreach (int value in ar)
+            {
+                if (dict.ContainsKey(value))
+                {
+                    dict[value]++;
+                }
+                else
+                {
+                    dict[value] = 1;
+                }
+            }
+            Console.WriteLine("Frequency of each element:");
+            foreach (var data in dict)
+            {
+                Console.WriteLine(data.Key + " appears " + data.Value + " times.");
+            }
+        }
+    }
+}
--- PracticeProblems/Program.cs
+++ PracticeProblems/Program.cs
@@ -10,7 +10,8 @@ namespace PracticeProblems
             while (flag)
             {
                 Console.WriteLine("Enter input to be executed\n 1.Count No.of duplicate elements in an array\n " +
-                    "2.Print all unique elements in an array\n 3.Exit\n");
+                    "2.Print all unique elements in an array\n 3.Count the frequency of each element of an array\n " +
+                    "4.Exit\n");
                 int input = Convert.ToInt32(Console.ReadLine());
                 switch (input)
                 {
@@ -23,6 +24,10 @@ namespace PracticeProblems
                         uniqueElements.PrintUniqueElements();
                         break;
                     case 3:
+                        Frequency frequency = new Frequency();
+                        frequency.CountFrequency();
+                        break;
+                    case 4:
                         flag = false;
                         break;
                 }
