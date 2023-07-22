12dfe095babc3876a188cd06cac0c35ae373abd0
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-07-22 16:43:52 +0530

    [Riya] Add : Count of Duplicate Elements

---
+++ PracticeProblems/DuplicateElements.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    public class DuplicateElements
+    {
+        public void CountDuplicatesElements()
+        {
+            Console.WriteLine("Enter the limit of array:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] ar = new int[n];
+            Console.WriteLine("Enter the elements:");
+            for (int i = 0; i < ar.Length; i++)
+            {
+                ar[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            var dict = new Dictionary<int, int>();
+            foreach (var value in ar)
+            {
+                if (dict.ContainsKey(value))
+                {
+                    dict[value]++;
+                }
+                else
+                { 
+                    dict[value] = 1;
+                }
+            }
+            int count = 0;
+            foreach (var data in dict)
+            {
+                if (data.Value > 1)
+                    count += data.Value;
+            }
+                    Console.WriteLine("Total number of duplicate elements: " + count);
+        }
+
+    }
+}
--- PracticeProblems/Program.cs
+++ PracticeProblems/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System;
+
+namespace PracticeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter input to be executed\n 1.Count No.of duplicate elements in an array\n " +
+                    "2.Exit\n");
+                int input = Convert.ToInt32(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        DuplicateElements duplicateElements = new DuplicateElements();
+                        duplicateElements.CountDuplicatesElements();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
